f4f6fb7682ac8ed61620e9a52b0d0d27028289f9
rakanoya112 <horseykate1129@icloud.com>
2024-08-21 11:33:00 -0700

    Update demo.java
    adding to demo

---
+++ demo.java
@@ -0,0 +1,26 @@
+package Software-ENG;
+import java.util.Scanner;
+import Software-ENG.saxophone;
+public class demo 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String instrument = input.nextLine();
+        System.out.print("Select alto or tenor");
+        if (instrument.equals("alto"))
+        {
+            alto altoSax = new alto();
+            altoSax.sound();
+        }
+        else if (input.equals("tenor"))
+        {
+            tenor tenorSax = new tenor();
+            tenorSax.sound();
+        }
+        else
+        {
+            System.out.print("You did not select one of the options");
+        }
+    }
+}
