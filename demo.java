ce98a5efa2bea2e89444181dea9f09729f36e275
rakanoya112 <horseykate1129@icloud.com>
2024-08-23 06:47:37 -0700

    Update demo.java
    more editing

--- demo.java
+++ demo.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 import Software-ENG.saxophone;
 public class demo 
 {
+    private saxophone sax1;
+    public demo()
+    {
+        sax1 = new saxophone();
+    }
+    public void setSax1(saxophone v)
+    {
+        sax1 = v;
+    }
+    public saxophone getSax1()
+    {
+        return sax1;
+    }
     public void foo(saxophone sax)
     {
         sax.makeSound();
@@ -25,6 +38,8 @@ public class demo
         else
         {
             System.out.print("You did not select one of the options");
+            System.out.println("Random sound will be played");
+            foo(sax1);
         }
     }
 }
